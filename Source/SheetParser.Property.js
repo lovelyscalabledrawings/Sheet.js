08e419f5d2aa7913cf2f35521d3d4d0a71896f5b
Yaroslaff Fedin <me@inviz.ru>
2011-01-16 20:11:52 +0800

    A more beefy (and consistent) CSS properties table

--- Source/SheetParser.Property.js
+++ Source/SheetParser.Property.js
@@ -29,35 +29,7 @@ provides : SheetParser.Property
   };
   
   
-  
-  var Styles = Property.Styles = {
-    "font": [
-      ['fontStyle', 'fontVariant', 'fontWeight'],
-      'fontSize',
-      ['/', 'lineHeight'],
-      'fontFamily'
-    ],
-  
-    "background": [
-       ["backgroundColor", "backgroundImage", "backgroundRepeat", "backgroundAttachment", "backgroundPosition"]
-    ],
-  
-    'boxShadow': [
-      'boxShadowBlur',
-      'boxShadowOffsetX',
-      'boxShadowOffsetY',
-      'boxShadowColor'
-    ],
-  
-    'textShadow': [
-      'textShadowBlur',
-      'textShadowOffsetX',
-      'textShadowOffsetY',
-      'textShadowColor'
-    ]
-  }
-
-  var Type = Property.Type = {
+  Property.Type = {
     length: function() {
   
     },
@@ -65,10 +37,113 @@ provides : SheetParser.Property
     color: function(obj) {
       return obj 
     },
+    
+    number: function() {
+      
+    },
+    
+    integer: function() {
+      
+    },
   
-    keywords: function() {
+    keyword: function() {
   
+    },
+    
+    strings: function() {
+      
+    },
+    
+    url: function() {
+      
+    },
+    
+    position: function() {
+      
     }
+  }
+  
+  var Styles = Property.Styles = {
+    background: [
+      ['backgroundColor', 'backgroundImage', 'backgroundRepeat', 'backgroundAttachment', 'backgroundPosition']
+    ],
+    backgroundColor: ['color', 'transparent', 'inherit'],
+    backgroundImage: ['url', 'none', 'inherit'],
+    backgroundRepeat: ['repeat', 'no-repeat', 'repeat-x', 'repeat-y', 'inherit'],
+    backgroundAttachment: ['fixed', 'scroll', 'inherit'],
+    backgroundPosition: [['backgroundPositionX', 'backgroundPositionY']],
+    backgroundPositionX: ['position', 'inherit'],
+    backgroundPositionY: ['position', 'inherit'],
+    
+    outline: ['outlineWidth', 'outlineStyle', 'outlineColor'],
+    outlineWidth: ['length'],
+    outlineStyle: ['dotted', 'dashed', 'solid', 'double', 'groove', 'reidge', 'inset', 'outset'],
+    outlineColor: ['color'],
+        
+    font: [[
+      ['fontStyle', 'fontVariant', 'fontWeight'],
+      'fontSize',
+      ['/', 'lineHeight'],
+      'fontFamily'
+    ]],
+    fontStyle:      ['normal', 'italic', 'oblique', 'inherit'],
+    fontVariant:    ['normal', 'bold', 'inherit'],
+    fontWeight:     ['number', 'normal', 'bold', 'inherit'],
+    fontFamily:     ['strings', 'inherit'],
+    fontSize:       ['length', 'percentage', 'inherit', 
+                     'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large', 'smaller', 'larger'],
+                    
+    color:          ['color'],
+    letterSpacing:  ['normal', 'length', 'inherit'],
+    textDecoration: ['capitalize', 'uppercase', 'lowercase', 'none'],
+    textAlign:      ['left', 'right', 'center', 'justify'],
+    textIdent:      ['length', 'percentage'],                 
+    lineHeight:     ['normal', 'length', 'number', 'percentage'],
+    
+    height:     ['length', 'auto'],
+    maxHeight:  ['length', 'auto'],
+    minHeight:  ['length', 'auto'],
+    width:      ['length', 'auto'],
+    maxWidth:   ['length', 'auto'],
+    minWidth:   ['length', 'auto'],
+    
+    display:    ['inline', 'block', 'list-item', 'run-in', 'inline-block', 'table', 'inline-table', 'none', 
+                'table-row-group', 'table-header-group', 'table-footer-group', 'table-row', 
+                'table-column-group', 'table-column', 'table-cell', 'table-caption'],
+    visibility: ['visible', 'hidden'],
+    float:      ['left', 'right', 'none'],
+    clear:      ['none', 'left', 'right', 'both', 'inherit'],
+    overflow:   ['visible', 'hidden', 'scroll', 'auto'],
+    position:   ['static', 'relative', 'absolute', 'fixed'],
+    top:        ['length', 'auto'],
+    left:       ['length', 'auto'],
+    right:      ['length', 'auto'],
+    bottom:     ['length', 'auto'],
+    zIndex:     ['integer'],
+    cursor:     ['auto', 'crosshair', 'default', 'hand', 'move', 'e-resize', 'ne-resize', 'nw-resize', 
+                 'n-resize', 'se-resize', 'sw-resize', 's-resize', 'w-resize', 'text', 'wait', 'help'],
+                 
+    textShadow: [[
+      'textShadowBlur',
+      'textShadowOffsetX',
+      'textShadowOffsetY',
+      'textShadowColor'
+    ]],
+    textShadowBlur:    ['length'],
+    textShadowOffsetX: ['length'],
+    textShadowOffsetY: ['length'],
+    textShadowColor:   ['color'],
+
+    boxShadow: [[
+      'boxShadowBlur',
+      'boxShadowOffsetX',
+      'boxShadowOffsetY',
+      'boxShadowColor'
+    ]],
+    boxShadowBlur:    ['length'],
+    boxShadowOffsetX: ['length'],
+    boxShadowOffsetY: ['length'],
+    boxShadowColor:   ['color']
   }
 
   var expanded = ['borderWidth', 'borderColor', 'borderStyle', 'padding', 'margin'];
@@ -77,21 +152,21 @@ provides : SheetParser.Property
       ['border' + side + 'Width', 'border' + side + 'Style', 'border' + side + 'Color']
     ];
   
-    Styles['border' + side + 'Width'] = Type.length;
-    Styles['border' + side + 'Style'] = Type.keywords('solid', 'dotted', 'dashed');
-    Styles['border' + side + 'Color'] = Type.color;
+    Styles['border' + side + 'Width'] = ['length', 'thin', 'thick', 'medium'];
+    Styles['border' + side + 'Style'] = ['none', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset', 'inherit', 'none'];
+    Styles['border' + side + 'Color'] = ['color'];
   
-    Styles['margin' + side] = Type.length;
-    Styles['padding' + side] = Type.length;
+    Styles['margin' + side] = ['length', 'percentage', 'auto'];
+    Styles['padding' + side] = ['length', 'percentage', 'auto'];
   
     for (var j = 0, prop; prop = expanded[j++];) {
       if (!Styles[prop]) Styles[prop] = []
-      Styles[prop].push(prop + '-' + side)
+      Styles[prop].push(prop + side)
     }
   
     if (i % 2 == 0) 
       for (var j = 1, prop, adj; adj = sides[j+=2];) 
-        Styles[prop = 'borderRadius' + side + adj] = Type.length;
+        Styles[prop = 'borderRadius' + side + adj] = ['length', 'none'];
   }
   
 })(typeof exports != 'undefined' ? exports : this);
